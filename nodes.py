9244ce94ca040d7849d1a13437f3a2daca020ee8
mirkocuchan <mirko.cuchan01@gmail.com>
2026-06-23 22:36:56 +0000

    activation function for the output layer, softmax. forward method

--- nodes.py
+++ nodes.py
@@ -17,14 +17,29 @@ class Activation_ReLU:
     def forward(self, inputs):
         self.output = np.maximum(0, inputs)
 
+class Activation_Softmax:
+    def forward(self, inputs):
+        exp_values = np.exp(inputs - np.max(inputs, axis=1, keepdims=True))
+        probabilities = exp_values / np.sum(exp_values, axis=1, keepdims=True)
+        self.output = probabilities
 
 #create dataset, 100 feature sets and 3 classes and each feature set has 2 fetures, like (a, b) = featureSet1 (we have 300)
 X, y = spiral_data(samples=100, classes=3)
 #create Dense layer with 2 input features and 3 output values
 layer1 = Layer_Dense(2, 3)
+#create ReLU activation (to be used with Dense layer):
 activation1 = Activation_ReLU()
+
+layer2 = Layer_Dense(3, 3) #lo tomamos como output layer y decimos 3 por las 3 clases 
+activation2 = Activation_Softmax() 
 
 #perform a forward pass of our training data through this layer
 layer1.forward(X)
+#forward pass through activation func. #takes in output from previous layer
 activation1.forward(layer1.output)
+
+layer2.forward(activation1.output)
+activation2.forward(layer2.output)
+
+#ahora tenemos el output de la softmax, probabilities
 
