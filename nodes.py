83dde216aa7e76a6772f4936225d42ddccaaf74d
mirkocuchan <mirko.cuchan01@gmail.com>
2026-06-27 01:37:59 +0000

    backward pass, backpropagation

--- nodes.py
+++ nodes.py
@@ -12,10 +12,25 @@ class Layer_Dense:
 
     def forward(self, inputs):
         self.output = np.dot(inputs, self.weights) + self.biases
-
+    # Backward pass
+    def backward(self, dvalues):
+        # Gradients on parameters
+        self.dweights = np.dot(self.inputs.T, dvalues)
+        self.dbiases = np.sum(dvalues, axis=0, keepdims=True)
+        # Gradient on values
+        self.dinputs = np.dot(dvalues, self.weights.T)
 class Activation_ReLU:
     def forward(self, inputs):
         self.output = np.maximum(0, inputs)
+    
+        # Backward pass
+    def backward(self, dvalues):
+        # Since we need to modify the original variable,
+        # let's make a copy of the values first
+        self.dinputs = dvalues.copy()
+        # Zero gradient where input values were negative
+        self.dinputs[self.inputs <= 0] = 0
+
 
 class Activation_Softmax:
     def forward(self, inputs):
