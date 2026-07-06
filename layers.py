b64ac9141421de8318151ca26733ce4ea46969cc
mirkocuchan <mirko.cuchan01@gmail.com>
2026-07-06 16:02:34 +0000

    l1 and l2 regs for the backward pass, next is dropout reg

--- layers.py
+++ layers.py
@@ -10,7 +10,8 @@ class Layer_Dense:
         self.weight_regularizer_l2 = weight_regularizer_l2
         self.bias_regularizer_l1 = bias_regularizer_l1
         self.bias_regularizer_l2 = bias_regularizer_l2
-
+        #L1 mata exactamente los pesos que no aportan nada a la predicción, y deja vivos los que sí aportan. L2 en ese caso ayuda 
+        #a que los que sobreviven no sean enormes. L1 decide quién vive y quién muere, L2 controla el tamaño de los que viven.
 
     def forward(self, inputs):
         self.output = np.dot(inputs, self.weights) + self.biases
@@ -21,5 +22,24 @@ class Layer_Dense:
         #gradients on parameters
         self.dweights = np.dot(self.inputs.T, dvalues)
         self.dbiases = np.sum(dvalues, axis=0, keepdims=True)
+        
+        #gradients on regularization
+        #L1 on weights
+        if self.weight_regularizer_l1 > 0:
+            dL1 = np.ones_like(self.weights)
+            dL1[self.weights < 0] = -1
+            self.dweights += self.weight_regularizer_l1 * dL1
+        #L2 on weights
+        if self.weight_regularizer_l2 > 0:
+            self.dweights += 2 * self.weight_regularizer_l2 * self.weights
+        #L1 on biases
+        if self.bias_regularizer_l1 > 0:
+            dL1 = np.ones_like(self.biases)
+            dL1[self.biases < 0] = -1
+            self.dbiases += self.bias_regularizer_l1 * dL1
+        #L2 on biases
+        if self.bias_regularizer_l2 > 0:
+            self.dbiases += 2 * self.bias_regularizer_l2 * self.biases
+        
         #gradient on values
         self.dinputs = np.dot(dvalues, self.weights.T)
--- main.py
+++ main.py
@@ -8,11 +8,11 @@ from losses import Activation_Softmax_Loss_CategoricalCrossentropy
 from optimizers import Optimizer_Adam
  
 nnfs.init()
-
+#training
 #create dataset, 100 feature sets and 3 classes and each feature set has 2 fetures, like (a, b) = featureSet1 (we have 300)
 X, y = spiral_data(samples=100, classes=3)
 #create Dense layer with 2 input features and 64 output values
-layer1 = Layer_Dense(2, 64)
+layer1 = Layer_Dense(2, 64, weight_regularizer_l2=5e-4, bias_regularizer_l2=5e-4)
 #create ReLU activation (to be used with Dense layer):
 activation1 = Activation_ReLU()
 
@@ -55,7 +55,7 @@ for epoch in range(10001):
     loss = loss_activation.forward(layer2.output, y)
 
     #calculate regularization penalty
-    regularization_loss = loss_function.regularization_loss(dense1) + loss_function.regularization_loss(dense2)
+    regularization_loss = loss_function.regularization_loss(layer1) + loss_function.regularization_loss(layer2)
     #calculate overall loss
     loss = data_loss + regularization_loss
 
@@ -68,7 +68,8 @@ for epoch in range(10001):
     #comparamos predicciones con el truth value y ponele queda algo así: [0,0,1] == [0,1,1] eso es [True, False, True], promedio de esto
 
     if not epoch % 100:
-        print(f'epoch: {epoch}, ' + f'acc: {accuracy:.3f}, ' + f'loss: {loss:.3f}' + f'lr: {optimizer.current_learning_rate}')
+        print(f'epoch: {epoch}, ' + f'acc: {accuracy:.3f}, ' + f'loss: {loss:.3f}' + f'reg_loss: {regularization_loss:.3f}), ' +
+         f'lr: {optimizer.current_learning_rate}')
 
     #backward pass
     loss_activation.backward(loss_activation.output, y)
@@ -82,3 +83,22 @@ for epoch in range(10001):
     optimizer.update_params(layer1)
     optimizer.update_params(layer2)
     optimizer.post_update_params()
+
+
+#validating the model
+#create test dataset
+X_test, y_test = spiral_data(samples=100, classes=3)
+#perform a forward pass of our testing data through this layer
+dense1.forward(X_test)
+#perform a forward pass through activation function, takes the output of first dense layer here
+activation1.forward(dense1.output)
+#perform a forward pass through second Dense layer, takes outputs of activation function of first layer as inputs
+dense2.forward(activation1.output)
+#perform a forward pass through the activation/loss function, takes the output of second dense layer here and returns loss
+loss = loss_activation.forward(dense2.output, y_test)
+#calculate accuracy from output of activation2 and targets, calculate values along first axis
+predictions = np.argmax(loss_activation.output, axis=1)
+if len(y_test.shape) == 2:
+    y_test = np.argmax(y_test, axis=1)
+accuracy = np.mean(predictions==y_test)
+print(f'validation, acc: {accuracy:.3f}, loss: {loss:.3f}')
