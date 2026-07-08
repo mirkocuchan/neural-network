e2a9b0d131ac58aef4123b5efec5485c3b969368
mirkocuchan <mirko.cuchan01@gmail.com>
2026-07-08 23:31:12 +0000

    binary classification implemented, validation and full code

--- main.py
+++ main.py
@@ -3,14 +3,19 @@ import nnfs
 from nnfs.datasets import spiral_data
 
 from layers import Layer_Dense, Layer_Dropout
-from activations import Activation_ReLU
-from losses import Activation_Softmax_Loss_CategoricalCrossentropy
+from activations import Activation_ReLU, Activation_Sigmoid
+from losses import Activation_Softmax_Loss_CategoricalCrossentropy, Loss_BinaryCrossentropy
 from optimizers import Optimizer_Adam, Optimizer_SGD
  
 nnfs.init()
 #training
 #create dataset, 100 feature sets and 3 classes and each feature set has 2 fetures, like (a, b) = featureSet1 (we have 300)
 X, y = spiral_data(samples=100, classes=3)
+    #create dataset for Binary Logistic Regression and Binary Cross-Entropy Loss
+    #X, y = spiral_data(samples=100, classes=2)
+    #reshape labels to be a list of lists, inner list contains one output (either 0 or 1) per each output neuron, 1 in this case
+    #y = y.reshape(-1, 1)
+
 #create Dense layer with 2 input features and 64 output values
 layer1 = Layer_Dense(2, 64, weight_regularizer_l2=5e-4, bias_regularizer_l2=5e-4)
 #create ReLU activation (to be used with Dense layer):
@@ -18,7 +23,15 @@ activation1 = Activation_ReLU()
 
 #create dropout layer
 dropout1 = Layer_Dropout(0.1)
-
+    #create second Dense layer with 64 input features (as we take output of previous layer here) and 1 output value
+    #if we are doing binary classification, we dont use dropout in the last layer.
+    #dense2 = Layer_Dense(64, 1) to do binary classification, we need 1 output neuron, not 3.
+    #create Sigmoid activation:
+    #activation2 = Activation_Sigmoid()
+    #create loss function for binary classification
+    #loss_function = Loss_BinaryCrossentropy()
+    #create optimizer, we are using Adam optimizer, which is a combination of RMSprop and Momentum. It adapts the learning rate for each parameter. Same as before
+    #optimizer = Optimizer_Adam(decay=5e-7)
 #create second Dense layer with 64 input features (as we take output of previous layer here) and 3 output values
 layer2 = Layer_Dense(64, 3) #3 output values (output values)
 
@@ -30,12 +43,13 @@ loss_activation = Activation_Softmax_Loss_CategoricalCrossentropy()
     #create loss function
     #loss_function = Loss_CategoricalCrossentropy()
 
-#create optimizer
-#optimizer = Optimizer_SGD(decay=1e-3, momentum=0.9)
-#adagard optimizer option
-#optimizer = Optimizer_Adagrad(decay=1e-4)
-#rms prop optimizer opotion
-#optimizer = Optimizer_RMSprop(learning_rate=0.02, decay=1e-5, rho=0.999)
+    #create optimizer
+    #optimizer = Optimizer_SGD(decay=1e-3, momentum=0.9)
+    #adagard optimizer option
+    #optimizer = Optimizer_Adagrad(decay=1e-4)
+    #rms prop optimizer opotion
+    #optimizer = Optimizer_RMSprop(learning_rate=0.02, decay=1e-5, rho=0.999)
+
 #optimizer adam option
 optimizer = Optimizer_Adam(learning_rate=0.05, decay=5e-5) #(best one so far)
 
@@ -64,7 +78,12 @@ for epoch in range(10001):
     regularization_loss = loss_activation.loss.regularization_loss(layer1) + loss_activation.loss.regularization_loss(layer2)
     #calculate overall loss
     loss = data_loss + regularization_loss
-
+    
+        #calculate accuracy from output of activation2 and targets
+        #part in the brackets returns a binary mask - array consisting of True/False values, multiplying it by 1 changes it into array of 1s and 0s
+        #predictions = (activation2.output > 0.5) * 1, clasifico las probabilidades de la softmax en 0 o 1, si es mayor a 0.5 es 1, sino es 0. Esto es para binary classification, no para multiclass.
+        #accuracy = np.mean(predictions==y), es lo correcto o no, devuelve un array de True/False, np.mean lo convierte en un porcentaje de aciertos, cuantos hizo bien de todos los samples. np.mean(True) = 1, np.mean(False) = 0, np.mean([True, False, True]) = 0.6667
+    
     #calculate accuracy from output of activation2 and targets, calculate values along first axis
     predictions = np.argmax(loss_activation.output, axis=1)
     #miramos cada fila (cada muestra) y elegimos la clase con mayor probabilidad
@@ -109,3 +128,24 @@ if len(y_test.shape) == 2:
     y_test = np.argmax(y_test, axis=1)
 accuracy = np.mean(predictions==y_test)
 print(f'validation, acc: {accuracy:.3f}, loss: {loss:.3f}')
+
+    #validate the model but for binary classification
+    #create test dataset
+    #X_test, y_test = spiral_data(samples=100, classes=2)
+    #reshape labels to be a list of lists, inner list contains one output (either 0 or 1) per each output neuron, 1 in this case
+    #y_test = y_test.reshape(-1, 1)
+
+    #perform a forward pass of our testing data through this layer
+    #dense1.forward(X_test)
+    #perform a forward pass through activation function, takes the output of first dense layer here
+    #activation1.forward(dense1.output)
+    #perform a forward pass through second Dense layer, takes outputs of activation function of first layer as inputs
+    #dense2.forward(activation1.output)
+    #perform a forward pass through activation function, takes the output of second dense layer here
+    #activation2.forward(dense2.output)
+    #calculate the data loss
+    #loss = loss_function.calculate(activation2.output, y_test)
+    #calculate accuracy from output of activation2 and targets part in the brackets returns a binary mask - array consisting of True/False values, multiplying it by 1 changes it into array of 1s and 0s
+    #predictions = (activation2.output > 0.5) * 1
+    #accuracy = np.mean(predictions==y_test)
+    #print(f'validation, acc: {accuracy:.3f}, loss: {loss:.3f}')
